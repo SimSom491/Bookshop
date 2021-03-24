ed284585bf3fcccf85ebb3f6182896d178f38add
Vörös Vivien <h986160@stud.u-szeged.hu>
2021-03-24 15:52:56 +0100

    spring alapok

---
+++ src/main/java/com/example/konyvesbolt/KonyvesboltApplication.java
@@ -0,0 +1,18 @@
+package com.example.konyvesbolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonyvesboltApplication {
+
+    /**
+     * MIELOTT MEGIJEDSZ, A TEMPLATE NEM LATSZIK A REPOBAN, 15.SORT KELL ATIRNI MAJD
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonyvesboltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/konyvesbolt/KonyvesboltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.konyvesbolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonyvesboltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
