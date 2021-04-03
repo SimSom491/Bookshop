bc89199358c373391bb3e14f5275b82bf75025d0
Vörös Vivien <h986160@stud.u-szeged.hu>
2021-04-03 21:04:22 +0200

    create

--- src/main/java/com/example/konyvesbolt/controller/VarsarloController.java
+++ src/main/java/com/example/konyvesbolt/controller/VarsarloController.java
@@ -1,11 +1,15 @@
 package com.example.konyvesbolt.controller;
 
 
+import com.example.konyvesbolt.dao.AjandekDAO;
+import com.example.konyvesbolt.dao.VasarloDAO;
 import com.example.konyvesbolt.model.Ajandek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class VarsarloController {
+    @Autowired
+    AjandekDAO ajandekDAO;
 
 }
