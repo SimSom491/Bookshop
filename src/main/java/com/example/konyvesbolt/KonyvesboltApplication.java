63136d31daf5467ef4c324b200a6506b28099e71
Vörös Vivien <h986160@stud.u-szeged.hu>
2021-04-03 16:22:29 +0200

    create

--- src/main/java/com/example/konyvesbolt/KonyvesboltApplication.java
+++ src/main/java/com/example/konyvesbolt/KonyvesboltApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KonyvesboltApplication {
 
-    /**
-     * MIELOTT MEGIJEDSZ, A TEMPLATE NEM LATSZIK A REPOBAN, 15.SORT KELL ATIRNI MAJD
-     * @param args
-     */
-
     public static void main(String[] args) {
         SpringApplication.run(KonyvesboltApplication.class, args);
     }
