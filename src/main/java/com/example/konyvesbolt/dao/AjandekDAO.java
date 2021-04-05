685e8aaaac0a7206b87a914500e0115abc105143
Wolf Ferenc <h986172@stud.u-szeged.hu>
2021-04-06 00:06:06 +0200

    index.html-ben fejléc + toplista,újdonságok sok ikon, két kép

--- src/main/java/com/example/konyvesbolt/dao/AjandekDAO.java
+++ src/main/java/com/example/konyvesbolt/dao/AjandekDAO.java
@@ -1,10 +1,11 @@
 package com.example.konyvesbolt.dao;
 
 
+import com.example.konyvesbolt.model.Ajandek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import com.example.konyvesbolt.model.Ajandek;
+
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.List;
@@ -12,22 +13,22 @@ import java.util.List;
 
 @Repository
 public class AjandekDAO {
-   @Autowired
-   private DataSource dataSource;
-   private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
-   @PostConstruct
-   private void postConstruct() {
-      jdbcTemplate = new JdbcTemplate(dataSource);
-   }
+    @PostConstruct
+    private void postConstruct() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
-   public Ajandek load(String nev) {
-      List<Ajandek> ajandeks = jdbcTemplate.query("SELECT NEV, AR FROM AJANDEK", (rs, rowNum) -> new Ajandek(rs.getString("nev"), rs.getInt("ar")));
-      for (int i = 0; i < ajandeks.size(); i++)
-      {
-         System.out.println(ajandeks.get(i).getNev() + " jacsika is itt van " + ajandeks.get(i).getAr());
-      }
-      return ajandeks.get(0);
-   }
+    public Ajandek load(String nev) {
+        List<Ajandek> ajandeks = jdbcTemplate.query("SELECT NEV, AR FROM AJANDEK", (rs, rowNum) -> new Ajandek(rs.getString("nev"), rs.getInt("ar")));
+        for (int i = 0; i < ajandeks.size(); i++)
+        {
+            System.out.println(ajandeks.get(i).getNev() + " jacsika is itt van " + ajandeks.get(i).getAr());
+        }
+        return ajandeks.get(0);
+    }
 
 }
