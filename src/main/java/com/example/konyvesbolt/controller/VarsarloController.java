fdceecf391eef9466338f178646522a66227c20e
Simon Soma <somas491@gmail.com>
2021-04-06 13:54:53 +0200

    Ajándékok listázás törlés hozzáadás

--- src/main/java/com/example/konyvesbolt/controller/VarsarloController.java
+++ src/main/java/com/example/konyvesbolt/controller/VarsarloController.java
@@ -2,19 +2,40 @@ package com.example.konyvesbolt.controller;
 
 
 import com.example.konyvesbolt.dao.AjandekDAO;
+import com.example.konyvesbolt.model.Ajandek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class VarsarloController {
     @Autowired
     AjandekDAO ajandekDAO;
     @GetMapping(value = "/")
-    public String listDog(Model model) {
-        ajandekDAO.load("nev");
-        return "index";
+    public String ajandekList(Model model) {
+        //addAji("FERI",9999 ,"https//valid veboladcim"); beszuras teszt
+        model.addAttribute("ajandekok", ajandekDAO.ajitListaz()); //kiiratas
+        //ajandekDAO.ajitTorol("FERI"); torlesteszt
+
+        return "asd";
+    }
+    @PostMapping(value = "/addAji")
+    public String addAji(@RequestParam("nev") String nev, @RequestParam("ar") int ar, @RequestParam("path") String ut) {
+        Ajandek aji = new Ajandek(nev, ar, ut);
+        ajandekDAO.ajitHozzaad(aji);
+
+        return "redirect:/asd";
+    }
+
+    @GetMapping(value = "/deleteAji/{nev}")
+    public String deleteAji(@PathVariable("nev") String nev) {
+        ajandekDAO.ajitTorol(nev);
+
+        return "redirect:/asd";
     }
 
 }
--- src/main/java/com/example/konyvesbolt/dao/AjandekDAO.java
+++ src/main/java/com/example/konyvesbolt/dao/AjandekDAO.java
@@ -5,6 +5,8 @@ import com.example.konyvesbolt.model.Ajandek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -22,13 +24,22 @@ public class AjandekDAO {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public Ajandek load(String nev) {
-        List<Ajandek> ajandeks = jdbcTemplate.query("SELECT NEV, AR FROM AJANDEK", (rs, rowNum) -> new Ajandek(rs.getString("nev"), rs.getInt("ar")));
-        for (int i = 0; i < ajandeks.size(); i++)
-        {
-            System.out.println(ajandeks.get(i).getNev() + " jacsika is itt van " + ajandeks.get(i).getAr());
-        }
-        return ajandeks.get(0);
+    public List<Ajandek> ajitListaz() {
+        List<Ajandek> ajandeks = jdbcTemplate.query("SELECT * FROM AJANDEK", (rs, rowNum) -> new Ajandek(rs.getString("nev"), rs.getInt("ar"), rs.getString("eleresiUt")));
+
+        return ajandeks;
     }
 
+
+    public void ajitHozzaad(Ajandek aji) {
+        String sql = "INSERT INTO AJANDEK(NEV, AR, ELERESIUT) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, new Object[]{
+                aji.getNev(), aji.getAr(), aji.getEleresiUt() }
+    );
+    }
+
+    public void ajitTorol(String nev) {
+        String sql = "DELETE FROM AJANDEK WHERE nev='" + nev +"'";
+        jdbcTemplate.update(sql);
+    }
 }
