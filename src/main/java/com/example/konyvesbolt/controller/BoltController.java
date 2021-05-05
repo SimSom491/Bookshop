31d8f96914a1d4b8b21597a1910f866eff22fb7f
Wolf Ferenc <h986172@stud.u-szeged.hu>
2021-05-05 15:59:01 +0200

    index

--- src/main/java/com/example/konyvesbolt/controller/BoltController.java
+++ src/main/java/com/example/konyvesbolt/controller/BoltController.java
@@ -2,6 +2,7 @@ package com.example.konyvesbolt.controller;
 
 import com.example.konyvesbolt.dao.BoltDAO;
 import com.example.konyvesbolt.model.Bolt;
+import com.example.konyvesbolt.model.Vasarlo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class BoltController {
     @Autowired
     BoltDAO boltDAO;
 
     @GetMapping(value = "/bolt")
-    public String boltotListaz(Model model) {
+    public String boltotListaz(Model model, HttpSession httpSession) {
         model.addAttribute("boltok", boltDAO.listaz());
+        Vasarlo vasarlo = (Vasarlo) httpSession.getAttribute("logged_in_user");
+        if (vasarlo != null) {
+            System.out.println(vasarlo.isAdmine());
+            model.addAttribute("admine", vasarlo.isAdmine());
+        }
         return "rendelesinfo";
     }
     @GetMapping(value = "/addbolt")
@@ -31,6 +39,6 @@ public class BoltController {
     ){
         Bolt bolt = new Bolt(nev,elhelyezkedes,nyitvatartas);
         boltDAO.beszur(bolt);
-        return "redirect:/addbolt";
+        return "redirect:/bolt";
     }
 }
